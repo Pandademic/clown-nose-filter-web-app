848307e13def66302ef1d45f9145bc56f1bcc241
Pandademic <stewie.gupte@gmail.com>
2021-08-06 13:19:34 -0400

    FINAL,I think

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+     clown_nose=loadImage('https://i.postimg.cc/bY0Q0Gp2/NOSENOSE.png')
 }
 function setup(){
      canvas=createCanvas(300,300);
@@ -12,7 +14,10 @@ function setup(){
 }
 function draw(){
   image(video,0,0,300,300);
-
+  //fill(255,0,0);
+  //stroke(255,0,0);
+  //circle(noseX,noseY,20);
+  image(clown_nose,noseX,noseY,30,30);
 }
 function take_snapshot(){
     save('clownyYou.png');
@@ -23,8 +28,10 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log("nose x ="+results[0].pose.nose.x);
-        console.log("nose y="+results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x-10;
+        noseY=results[0].pose.nose.y-10;
+        console.log("nose x ="+noseX);
+        console.log("nose y="+noseY);
 
 
     }
